4dab8ecfc801c484667297375852030aa8534c1e
ademdurak <ademdurak@outlook.com>
2016-05-31 14:07:32 +0300

    Scorecard statu kolonuna resim ekleme

---
+++ SAKA/SAKA/BitMatImage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SAKA
+{
+    internal class BitMatImage
+    {
+        public BitMatImage()
+        {
+        }
+    }
+}
